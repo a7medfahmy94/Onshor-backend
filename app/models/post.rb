3a623083110529405649c84d762c03be7558b6f9
mohammed2049 <mohammed.shane94@gmail.com>
2015-10-24 12:02:38 +0200

    edit_validation_post_model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :posts_users
-  has_many :user , through: :posts_users
+  has_many :posts_userses
+  has_many :users , through: :posts_users
   validates :content , presence: true
+  validates :user_id , presence: true
   validates_associated :user
 end
