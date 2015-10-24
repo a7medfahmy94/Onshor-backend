d5a4028245b629c06dff005fe7beadfd17c5264f
mohammed2049 <mohammed.shane94@gmail.com>
2015-10-24 12:04:56 +0200

    fix_relation_user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   validates :gcm_registration_id , presence: true  
   validates :radius , presence: true
   has_many :posts , through: :posts_users
-  has_many :posts_users
+  has_many :posts_userses
 end
