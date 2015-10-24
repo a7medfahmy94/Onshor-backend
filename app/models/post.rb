eb92d8062a9881a73dbcb2a94d3fc4d243fc860f
mohammed2049 <mohammed.shane94@gmail.com>
2015-10-24 10:47:05 +0200

    add_relations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  has_many :posts_users
+  has_many :user , through: :posts_users
   validates :content , presence: true
-  validates :number_of_ignores , presence: true
-  validates :number_of_shares , presence: true
   validates_associated :user
 end
