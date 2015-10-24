1164daee35e2bf69baf7cfb5151c6ba68791aad5
mohammed2049 <mohammed.shane94@gmail.com>
2015-10-24 10:47:46 +0200

    add_relation_in_user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   validates :device_id , presence: true
   validates :gcm_registration_id , presence: true  
-  has_many :posts
+  validates :radius , presence: true
+  has_many :posts , through: :posts_users
+  has_many :posts_users
 end
