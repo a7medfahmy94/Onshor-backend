42e46c728b2985d26959f9c898c17f14bf22daf6
Ahmed Fahmy <a7med.fahmy94@gmail.com>
2016-05-12 15:12:30 +0200

    make the users show method handle new/old users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,21 +9,17 @@ class UsersController < ApplicationController
     render json: @users
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
-    render json: @user
-  end
-
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if @user
+      render json: @user
     else
-      render json: @user.errors, status: :unprocessable_entity
+      @user = User.new(user_params)
+
+      if @user.save
+        render json: @user, status: :created, location: @user
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
     end
   end
 
@@ -50,11 +46,11 @@ class UsersController < ApplicationController
   private
 
     def set_user
-      @user = User.find(params[:id])
+      @user = User.where(device_id: params[:device_id]).first
     end
 
     def user_params
-      params.require(:user).permit(:device_id, :gcm_registration_id, 
+      params.require(:user).permit(:device_id, :gcm_registration_id,
         :radius, :number_of_shares, :number_of_ignores, :longitude, :latitude)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   apipie
   resources :posts, except: [:new, :edit]
-  resources :users, except: [:new, :edit]
+  resources :users, except: [:new, :edit, :create]
 end
