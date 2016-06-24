c807149aae6fa89ca5a879f9f200dcf00f4fcc8a
Amr <amr.essam67@stud.fci-cu.edu.eg>
2016-06-25 01:56:16 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :posts, except: [:new, :edit] do
     member do
       post :share
-      post :reply
     end
   end
   resources :users, only: [:show, :update, :destroy]
